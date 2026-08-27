906784d685c587fcf31381b96807f7b9062870b0
Eduardo Alves <eduardo.santos.alves14@gmail.com>
2026-08-27 10:52:21 -0300

    cucumber.cjs

--- cucumber.cjs
+++ cucumber.cjs
@@ -3,16 +3,30 @@ module.exports = {
     paths: [
       'tests/e2e/features/**/*.feature'
     ],
+
     requireModule: [
       'tsx/cjs'
     ],
+
     require: [
       'tests/e2e/steps/**/*.ts',
       'tests/e2e/support/**/*.ts'
     ],
+
     format: [
-      'progress'
+      'progress',
+      'allure-cucumberjs/reporter'
     ],
+
+    formatOptions: {
+      resultsDir: 'allure-results',
+      environmentInfo: {
+        framework: 'Cucumber.js + Playwright',
+        browser: 'Chromium',
+        node_version: process.version
+      }
+    },
+
     parallel: 1
   }
 };
